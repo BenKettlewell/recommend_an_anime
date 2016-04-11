622ff6c61673cf7d54100e67a02a833af30757b4
benkettlewell <ben.kettlewell@gmail.com>
2016-04-10 18:54:02 -0700

    Scaffolded Tag and Tagging and AR migrated

---
+++ app/views/taggings/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @tagging, :id, :tag_id, :anime_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411013152) do
+ActiveRecord::Schema.define(version: 20160411015214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,4 +39,24 @@ ActiveRecord::Schema.define(version: 20160411013152) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "anime_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "taggings", ["anime_id"], name: "index_taggings_on_anime_id", using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "tag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tags", ["tag"], name: "index_tags_on_tag", unique: true, using: :btree
+
+  add_foreign_key "taggings", "animes"
+  add_foreign_key "taggings", "tags"
 end
