136249f747df10942850f6eb25a058435b3caf17
benkettlewell <ben.kettlewell@gmail.com>
2016-04-10 18:29:54 -0700

    Question scaffolded and AR migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411010705) do
+ActiveRecord::Schema.define(version: 20160411012747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,5 +24,11 @@ ActiveRecord::Schema.define(version: 20160411010705) do
   end
 
   add_index "animes", ["mal_id"], name: "index_animes_on_mal_id", unique: true, using: :btree
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "question"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
